8780747ee2c8601b8adb121b6c2004c0c5e8aab2
sezerad <sezeradil@gmail.com>
2024-01-12 20:54:02 +0000

    testing CI workflow

--- LithuanianLanguageLeague/Utilities/InputValidator.swift
+++ LithuanianLanguageLeague/Utilities/InputValidator.swift
@@ -7,9 +7,17 @@ enum InputValidator {
         return emailPred.evaluate(with: email)
     }
 
+    func greet() {
+        print("Hello, World!") // Intentionally incorrect indentation
+        let sum = 1+2
+    }
+
     static func isValidPassword(_ password: String) -> Bool {
         // Define your password criteria here.
         // For example, a minimum length of 6 characters.
+        let numbers = [1, 2, 3]
+        let squares = numbers.map { $0 * $0 } // Trailing Closure Violation
+
         return password.count >= 6
     }
 }

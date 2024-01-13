85a4aba9d27fe19c6d6918e1586c82c64816c428
sezerad <sezeradil@gmail.com>
2024-01-13 00:48:40 +0000

    swiftlint format

--- LithuanianLanguageLeague/LithuanianLanguageLeagueApp.swift
+++ LithuanianLanguageLeague/LithuanianLanguageLeagueApp.swift
@@ -8,8 +8,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication,
-                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
-    {
+                     didFinishLaunchingWithOptions _:
+                     [UIApplication.LaunchOptionsKey: Any]?) ->
+        Bool {
         // Configure Firebase
         FirebaseApp.configure()
         return true
